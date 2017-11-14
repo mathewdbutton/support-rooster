b5275fcd70d2bf001c3e31ca40d8708d9b9f42ef
Mathew Button <mathewdbutton@gmail.com>
2017-11-14 21:59:08 +1100

    need to recompile to get env vars

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let staticServer = require('node-static');
-let file = new staticServer.Server();
+let file = new staticServer.Server('./build');
 require('http').createServer(function(request, response) {
     request.addListener('end', function() {
         file.serve(request, response);
