64a4ef97895e9c66f43be613688fc6270e1a8446
Mathew Button <mathewdbutton@gmail.com>
2017-11-19 17:29:09 +1100

    Check if we should invalidate cache?

--- src/utils/services/cache.js
+++ src/utils/services/cache.js
@@ -1,13 +1,15 @@
 import request from './request'
+import moment from 'moment'
 
 class Cache {
 
     static make_request(options) {
         let storageKey = options.baseURL
-        if(!localStorage.getItem(storageKey)) {
+        //Checking if key and time are set OR if it's been a day since we made a request
+        if((!localStorage.getItem(storageKey) && !localStorage.getItem(`${storageKey}_TIME`)) || moment().diff(moment(localStorage.getItem(`${storageKey}_TIME`),"DD-MM"),'days') > 0 ) {
             return request(options).then((response) => {
                 localStorage.setItem(storageKey, JSON.stringify(response))
-                localStorage.setItem(`${storageKey}_TIME`,Date.now())
+                localStorage.setItem(`${storageKey}_TIME`,moment().format("DD-MM"))
                 return response
             })
         } else {
